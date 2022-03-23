a329d20c05b2819395e2dc4f546f8ef7696c844a
Nikita Astapenko <astapenko-nik@mail.ru>
2022-03-23 20:22:23 +0700

    added .py files

---
+++ python lesson2.3_step4.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from math import *
+
+try:
+    link = "http://suninjuly.github.io/alert_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    alert = browser.switch_to.alert
+    alert.accept()
+
+    x = browser.find_element_by_id("input_value").text
+    summ = log(abs(12*sin(int(x))))
+
+    pole = browser.find_element_by_id("answer")
+    pole.send_keys(str(summ))
+
+    button2 = browser.find_element_by_css_selector("button.btn")
+    button2.click()
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
